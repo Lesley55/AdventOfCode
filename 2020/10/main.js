8d1031507bd5ca3aa9738ff5eee0421908f2737d
Lesley <hoiLesley55@gmail.com>
2020-12-06 13:49:17 +0100

    alvast nieuwe lege folders

---
+++ 2020/10/main.js
@@ -0,0 +1,14 @@
+let input = []
+
+function start() {
+    let total = 0
+    for (let i = 0; i < input.length; i++) {
+        
+    }
+    console.log(total)
+}
+
+start();
+
+// eerste deel: 
+// tweede deel: 3290
