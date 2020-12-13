dbb719768e543f59f2dce40c587681935a9528b7
Lesley <hoiLesley55@gmail.com>
2020-12-13 14:25:23 +0100

    20 min verder nog niet klaar met runnen, tijd voor performance upgrade denk ik

--- 2020/13/main.js
+++ 2020/13/main.js
@@ -1,11 +1,13 @@
 let time = 1002462
 let input = [37,"x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x",41,"x","x","x","x","x","x","x","x","x",601,"x","x","x","x","x","x","x","x","x","x","x",19,"x","x","x","x",17,"x","x","x","x","x",23,"x","x","x","x","x",29,"x",443,"x","x","x","x","x","x","x","x","x","x","x","x",13]
 let a = []
+let two = []
 
 function start() {
     for (let i = 0; i < input.length; i++) {
         if(input[i] !== "x") {
             a.push([input[i], input[i]])
+            two.push([input[i], input[i]], i)
         }
     }
     
@@ -21,6 +23,33 @@ function start() {
         }
     }
     console.log(bus[0] * (bus[1] - time))
+
+    // part 2
+    let t = input[0]
+    while(true) {
+        for (let i = 0; i < two.length; i++) {
+            while(two[i][1] < t + two[i][2]) {
+                two[i][1] += two[i][0]
+            }
+        }
+        let c = 0
+        for (let i = 0; i < two.length; i++) {
+            if(two[i][1] == t + two[i][2]) {
+                c++
+            }
+        }
+        if (c == two.length) {
+            break
+        }
+        t += input[0]
+        
+        // mogelijk performance gain als je grote getallen hebt of het eerste getal klein is en een niet te grote array om te sorten?
+        // let sort = two.sort(function(a, b) { return b[1] - a[1]; }); // sort groot naar klein
+        // let w = sort[0][0] / input[0] + 1
+        // t += input[0] * w
+        // je hoeft niet elke keer array te sorten, 1x de grootste pakken voor de while is beter
+    }
+    console.log(t);
 }
 
 start();
