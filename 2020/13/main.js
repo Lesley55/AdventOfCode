5b48491b15805e48a62e99a76594dcc9cb489d0c
Lesley <hoiLesley55@gmail.com>
2020-12-13 13:34:10 +0100

    2020 day 13 part 1

--- 2020/13/main.js
+++ 2020/13/main.js
@@ -1,14 +1,29 @@
-let input = []
+let time = 1002462
+let input = [37,"x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x","x",41,"x","x","x","x","x","x","x","x","x",601,"x","x","x","x","x","x","x","x","x","x","x",19,"x","x","x","x",17,"x","x","x","x","x",23,"x","x","x","x","x",29,"x",443,"x","x","x","x","x","x","x","x","x","x","x","x",13]
+let a = []
 
 function start() {
-    let total = 0
     for (let i = 0; i < input.length; i++) {
-        
+        if(input[i] !== "x") {
+            a.push([input[i], input[i]])
+        }
     }
-    console.log(total)
+    
+    for (let i = 0; i < a.length; i++) {
+        while(a[i][1] < time) {
+            a[i][1] += a[i][0]
+        }
+    }
+    let bus = a[0]
+    for (let i = 1; i < a.length; i++) {
+        if(a[i][1] < bus[1]) {
+            bus = a[i]
+        }
+    }
+    console.log(bus[0] * (bus[1] - time))
 }
 
 start();
 
-// eerste deel: 6583
-// tweede deel: 3290
+// eerste deel: 3606
+// tweede deel: 
