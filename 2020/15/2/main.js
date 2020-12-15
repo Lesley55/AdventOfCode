26d824df3c93998cf01a37f7f5beb7c869a61a5c
Lesley <hoiLesley55@gmail.com>
2020-12-15 21:13:20 +0100

    hij neemt laaste ook mee, maar dat moet niet, idk, zie niet echt hoe ik dit moet optimaliseren zodat het 30000000 aankan

--- 2020/15/2/main.js
+++ 2020/15/2/main.js
@@ -7,7 +7,7 @@ function start() {
     }
 
     let last = input[input.length-1]
-    for (let i = input.length; i < 2020; i++) {
+    for (let i = input.length; i < 30000000; i++) {
         let a = unique.findIndex((e) => e[0] == last)
         if(a !== -1) {
             last = i - unique[a][1]
