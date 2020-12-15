eb59cd6e8a0c5c7bdcef449264682a25d2507db2
Lesley <hoiLesley55@gmail.com>
2020-12-15 17:36:43 +0100

    2020 day 15 part 1

--- 2020/15/main.js
+++ 2020/15/main.js
@@ -1,14 +1,24 @@
-let input = []
+let input = [0,8,15,2,12,1,4]
 
 function start() {
-    let total = 0
-    for (let i = 0; i < input.length; i++) {
-        
+    while(input.length < 2020) {
+        let last = input[input.length-1]
+        let found = false
+        for (let j = input.length-2; j >= 0; j--) {
+            if (last == input[j]) {
+                input.push(input.length - 1 - j)
+                found = true
+                break;
+            }
+        }
+        if (!found) {
+            input.push(0)
+        }
     }
-    console.log(total)
+    console.log(input[input.length-1])
 }
 
 start();
 
-// eerste deel: 
+// eerste deel: 289
 // tweede deel: 
