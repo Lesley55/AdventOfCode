a29c0ed94fa1a95b44d9fd7f8a5c609285cbab05
Lesley <hoiLesley55@gmail.com>
2020-12-24 13:07:15 +0100

    2020 day 24 part 1

--- 2020/24/main.js
+++ 2020/24/main.js
@@ -1,14 +1,84 @@
-let input = []
+let input = ["seseswnwseneweseswseswnwnese", "neneeneseneeeesenewwne", "swswswneswsewswseswesewswseswneeswswnwsw", "eneeswnwneneenwneneneeneneswnwneswne", "seeseseeeeseenenenwswseweese", "seswweseseseswseswneswwnwswswseneswnw", "wswseswswneeswswsw", "wnewnwnewwwwwwwwwsewwsw", "wseswnenwneswwnwseeenwnwnwewesewse", "seweseseneeeseeswsenwseeenwseeeew", "seswswnwswswswnesesesewseeneseswsesesesw", "wsenewsenwwnewswwewnwnwsewwwsw", "swwwnewnwwseswswnwswenwswseeswesw", "senenwswwnwnewwseseseswswwweneswwne", "nwseseeeesweee", "swwswswswwswswsweswnwwswsw", "sewnwswneneneenenewneeneeneneswwee", "swenenwswnwnenwnwnwenwnenesenwnenwnwnwwnw", "eweeneswnesenesewneneeeneseneneew", "enesesenwsweeeeeenwnwnwsweswew", "nweeeswneswenwseswnweeeneenenesw", "neeeswnwnenwnewnwnwswenwwwnwsesenw", "neswnwenwneswnwnwnwneswnesenwnw", "neeseeweneeswweeswneneneeneenenw", "nenwnenwnenwewnewsenwnwnwnwnene", "nwewnwnwwnenwnwwnwnwwnwwenwnwsenw", "seseswsewneswseseseseseswwseseswe", "eseswnewenwwnenene", "wswneneswswsewnwseswswswseswswwwswswne", "neeswneneewnenenenenwnewneeeneneswse", "eneeneneneneeeneneenewsene", "eneweewwsenesene", "nwenwenwnwswnenwsenwnesenenenenwnesw", "wesenesenewweewwwswwnwsew", "eeswneeewsewneneneweneeneene", "eeswswswnwweswnenwsw", "swesewnwneswseesesesesesenwsenwesee", "enwsenwnwswnwwnwseseseseeswewnesenw", "eeseeeseenwseenweeeenwesese", "nenwnwswneneswneenenwneneswnenwnwnwnenee", "ewswwswwswewwnwwenww", "swnenenwnenenenesenenesewnenwnenwesenesw", "nwseeenwnwwnwnwenwswnwswnwnwneenwnew", "ewwnwwwwnwswnwnenwsenwnwewnwww", "eswwwseewsewweeswwnwnwwenwwwne", "wswseswswswnenwswwswswnewswswse", "nwnenwseseswsesesenwseseseweswew", "swesweenenwnwnenwnenwwneneseenenwsw", "wswwwswneswwwswwewnenwswwwew", "wsewwwnwwwwnwsenenewnwewwsw", "wswswwesweswnwswwwswswsweswswswsw", "eswswwnwwswsesweswswswneswswnwnwsesw", "neenwsewwswneseswwseswnwwwwswww", "enwseweseseseeswseneneseswseswneee", "swnwswswneseswswswsesewseseneneseswsee", "wsenwwswwwnwswwwwneswewwneww", "nweswsweeewnenesee", "swsweswnwswseneswswswseswswswsw", "swsewseswseeseneeswswwswswnenenwnesesw", "seeeenenewenenenenesew", "nwnesewsesesenwsesenwsenwnwnenwnenwnww", "enwswsewnesesesenwe", "nwnwnesenwwwwnwnw", "nenesenewneenenenenwswneenesenenwnewne", "wnwnesenenwesewsenenenenwneeeneswse", "wwswswswswswswwswwswesww", "enwsesesewseeesweneeeswesesweenw", "wwnwseneneneeenewswnwnwneswsenewnwsenw", "swswswswswswewswswwswsww", "nenwwnenwsenwnwswseswnwneneseseesenww", "seeseeseeeesenewseswweseseswnenese", "wwsewwwewneeswnwwwewwwenesw", "wenewseeeneneenenenenenwswswnenwnee", "neswnwnwesesenwswewneewsenwesweswswse", "nesweewswsewswnesewnewswwwwwsw", "nwneeeneseneneeneswenenwswnewsenwe", "swswswswnwswwswswesweeesenwswwswse", "senenwewnenenenenwnwneneswneenenenewne", "senwseneesweeesenenwseseseneewwe", "ewwwwwnesenesenwnwsewsesenenewswnw", "sewswswswseneswseseswseswneswswse", "eenwneneeeeeewswneseneeew", "swwwwseswswnweswwnwenenweenwseeene", "eeneeeeswnwneseenwesweeenweee", "enwwwwnwnwwwnwnwneseswswnwnwnwwenwnw", "wwwswswswswwswewswsw", "swwwwnesewswewwswneswseenw", "seeneeeeeweeeseee", "swneswwnwsenweswnweswswnwseeseseseswnw", "swswseswnwswswsweseswnwswswswneswsesew", "neneswswwwwwswwnwswswwswsesweswsesw", "sweneswnesenenwnenwesenewnwnewnwwene", "nwnwnenenwwnesweeswnwswseseneseewnw", "nenwnenenwneneneswneneenenwswnenw", "nwseswseswwwswnwneswswwswwwwswnese", "esenwenwnwwnewnwnwneswnwnwwnwnwnwnw", "wwwswsenwwswwwwwwnwnwwsewwe", "wesenweneeeseswseeneswswnenwwesw", "swswnwseseswsenwnenwnwseewweswswseene", "swswnewswswneswswswsenwweswwswswswsww", "eswwnenwnwneewenweswswnewweswese", "seneenwsewwnwnee", "swswswswsenwsweswswswenwswsenwswsesesw", "nwnweenwnwnenwnwneswswnwnwsenwnwnwnwnwne", "wnwwnwwwwwwsew", "nwswnwnwnenenwnwnwswnwseneswnwnwnwne", "newwneeeswseneeneneneneweeeenene", "eeenwnwneeeeeeesweenwswwseew", "esenewwwwnwswswswswwwnwswswswww", "eeweeeseseeesee", "wewwnenewswwneseswwnwwwseswswww", "wweenenewswswnwwewwswwwswswsww", "seseeswneesewnwsenwsesesesenwwswnwsee", "nwnwnenwnenwnwnwsesenwnwnwwnwnenenwnwse", "eswswseseswwswseseswswnweseswenwsesw", "eeseneneeesenenwswwweewneenene", "swsenwwswnwnwnwswnwenweneswneeneswnw", "nenwenenewseneweewswneenesweene", "swwwwwwwnwswew", "seesenwesesesewnwsenweseseswseswsee", "nenenwnwnenewnwnwnenwseswnwnwnwnenwseswse", "neneeeenwwweseseswewseesenwswnw", "enewesenenesenwwsesweneeneeneenenee", "seeeeeneewseewseseeeeesesewnw", "wwswswswswwwnwwewswsw", "wswnwnenweswnwnwnwnwnwnwsenwewse", "nwnwnwnwswnenenwenwenwnwneseswnwnwnenwne", "ewswswswwnwswswwswnwswswswswwwsesenwne", "wnwsenesenwsenwesewewseswnwnenwneswne", "nesweenenwneeneseeeneneseenewneenw", "nwswswswesenwsenwesweseswsewswesenwsw", "sweswneneeneswnwesenwwnwseeseswwnw", "enwnwnwnwnenwnwnwnenwwnwesewnwnwsenwnw", "eneseweeeneneneswwseneswweswswsw", "swswwwseneeseeewnwwseswnwswenwneswnw", "senewwswsesenewnwseseswswswneseneswsesw", "swnwnwnenwsenwsenwnwsenwnwwnwnwew", "seseseseswneesenweseeseswsesenwsewsese", "eewweeneneswswenenweneneeeesw", "ewneneeeseeeneneesweswenweswene", "wseeneseeswnwwesesesenwnwwsesee", "sesesesewseseneseseswnesesesenwsesenwse", "seseswseneneewseseseseseseesesee", "swswsesenwneeseswsesesesesesesew", "nenwseswswnesenewwnwenwseeeeneneenene", "neswneswswnewnwswnwseneeneneenwenwne", "swnwnwnwnwnwseenewnenenwnwnwnweewnwnw", "swnwnewnwwnwnwwnewswwsewwnwswnewew", "nwnenwswnenwnwnwnenesenwsenenwnenenesenwnw", "nenwewseswneweesweseeenweswswnw", "swsweswseeswwswswswneswwwnwnwswnwse", "swwwswwsewenewsewswswwnewnwswnw", "swenenwnweswnwswwswwwnwweswnewnw", "nwnenwnwnwnwwenenwnwsenenewenwnwswnesw", "neesewswnwswseswnwenenwwswswswswewse", "seseseseeeseseseswnw", "swswenwswnenwnwnenwnwnwsenenwnwnenenwnwnew", "wseseeeseseneseswesesesesese", "seswseseseseswneswnwwseseseswesw", "nwnwnwnwenweswsesenwswnwnwnwnwnwewswnwne", "swneneenenwnenenewneneeweeeeswnese", "wwnwnewwwwwswwww", "eseeeseeeeenwewseenwesenwwse", "seswseswneswseswswswwseswsw", "wneswseswswnwswneseswsweswneswwswswsesw", "wswwnwnwnwnwnesweswseswseswenwwswew", "neeeeswneeswnweeeeeeesweee", "neswswnwswesenwsenesenwsewseewswswne", "newseesesesenwsenwewsewsese", "sewseenwseseseswseseeswseseseneenwse", "weseneeeeeesewneseneeswewsew", "wswnwnwsewnwnwenwewwnwsesenwnenwnwnw", "senenenenenewnenwnwse", "seseseseseswsesenesesesesewsenwseneese", "swwneewwneswsewseseeneenwsenenese", "swnwnwneswseswsweswewneswnewnweseswsw", "swswswswseeswswnwseswswnw", "nenwseneenwneneenwneneneswseseeneswenw", "swneswswwswswseswswswswnweswewswswe", "swnenenwneswenenewnwnwnenenenese", "nwswswnewseswnwswnwwwesweeswesese", "nwenwnwnwnenwnwnesenwnwnwswnwnwsesenese", "sweewwnwswesewnenwswww", "eweneeswwswneeneeeseesenenwnee", "nwnwnwnwsewnwenwnwnweswwwnenwnwee", "seswswswwswnwwseneswswswswneswwswnene", "newnenenwnesenenesewneswnenwnwnwnenwne", "sewnenenenwneeswwenwneswnese", "swnwnwwnwnwnenwenenesenewneeswnenene", "swnenenesenewneneswsewwwwneesesee", "wewneneeneneswwwsenenesene", "wenwswswswnwwenwwwwenewnwnwwww", "wwewseswwwswswwwnewnewnwnwwwne", "swswwswswnwswnwswseswswswnwswswswseeswne", "wewswnweneeseseeweeeewwsene", "swsewwsenwseswewneswsesee", "seseswnwneneseesesesesesesenweewsesese", "seseseseswnesesesenwswsesew", "swnwseswewseswswswswswse", "wwesenwnwnweswswnwneewswnwneewse", "nwnwnwnwnwnesesenwnwsenewnwnwnwnwnwnwnw", "wswwwnenwswseswewwswswwnwwswswsw", "senenenewnenesenenenenenwenweese", "swneneeneswenewswnenwnweneesee", "neenenewneeewneneswseneseneneneene", "swnewswswsenwwswseswnweswswnwsweswsw", "ewwnwenwwwsewnwnwnwsesw", "seswseswswswseswsesenwsese", "neneswnweeeeeneeswwnwswseee", "swnenwnenwnwneswenenenwnenenweneneswnee", "nenenwnwneneneneswnenenwnwsenwnewesenwwe", "wwnwnwnenwwwwewseeswwwnwnwswnwnw", "neneeneeeeneneeeeewe", "nwneswweswswnesweesenwswswswswsesewse", "seewseneseneseseseswswewseeseenwsee", "nwswseswswseseswseswseswse", "swseewnwseswwwseeswswnesewneneese", "nwneeseeeneeeeeeee", "wwswenwwnwnwwwsenwwwwwnwsenew", "weneneneeneneswne", "nenwswneswswwswwwwwswenewwsesewsw", "sesesenenwneswwenesenewesweswseeenw", "sweewswwnwenwseseseenwsesw", "enwneenwewsenwnwwsewwwsww", "nwnweseneswneeneneneweswswwneseswnese", "nweseswneeseenwnenenesesenwenenenwwe", "nwwnwnwsenwnwnwenenwnwnwsenwswnwsenwwnw", "wewwwwseswnwsewwwnenwwwwsenesw", "seswseswswswswseseswnwswswesw", "sweswwnewneneneneneseneneneenwenene", "swwnwswswwwewswswenwwse", "senwenwnwwnwesesenwwnwnwnenwsenwnwnwne", "seseeeweseswwnenwsenesewnweseeese", "nwwewwnwnenwswnwsewswwnewenwnwnwnw", "eesweesenweewneeeeenwsweene", "newswesenwnewswneswswswseswswnwswnesw", "sesesewwsesewseseneeswsenesesenesese", "nwnwwswnwnenwwnwwnwnwneseswesenwswnwse", "wwnesewnwnesesewsewnwwneneswse", "eeweneeneweneeseeewneeeene", "neneswnenwnenenwnenenwnwnwene", "wwwseswswneenewseswseswwswnenesese", "nwnwnwnwseswwnwswnwnwsenwnwsenwsenenwnee", "newswswwwwseseneswwswswnewswwww", "seseseseneseseswswswseseesenesesewswnwse", "eeseswweseeenwsenesesesesesewsesee", "neenenenenenwnweesenwesenwenesenesee", "nwnwnwnenwswnwnwsenwnwnwnenewnwnw", "seswnewswswswswnwe", "eswnewneeneneneenenenwnenewnwewswse", "wnewnenenwnwseneneseswsewenwsesenwwwse", "eeseseweneseseewnesewnesesesenw", "seeneeeewseeeneewsesweeseee", "seseseswswsenwneeswnwswseseseswseswswnese", "sewwswswseneswnwwwwswwne", "nwnwnwnenwnwnwwnwnweswnwnwswenw", "seswnwseseswnwnwseseenenw", "newsewswnwwwneswsewneeeseswnwwenenw", "neneeswnenenewseesenewe", "seswneswsesewseeswse", "sesenwnewneneeeneneneenenenesewnenenenw", "wwneneswewswswsew", "sewwwnwnwnwwsenwnenwsesenww", "nwwnwnenenwwnwwswswswewseneesenewwnw", "eeswswneneneneeneeeeseeewneee", "wsewswwwnenwswnwswwsenenwenenw", "nwwnwwwwnwewnewwwsenwswwswenwne", "swsenesenwsesesewseswneeseseseseswsenw", "wswswswseeswswswswnwswneweseswswswswnw", "wnwnwwnwnwwnwnwwnwwswnwwenwwese", "swseesenwswnwsenwswsweeweenwneeenene", "neswnenewneneneneneneneneswnesenenesew", "wnwneenenewnenenesenwne", "senwnwseenesewseswwseswsweswsenenwsee", "nwnwnwwnwenwsweswwnwseenwwnwnwnwnw", "neseneneneswnwswsenewnewneenwsewnenenese", "nwseseswwswseesenesenese", "swsenwsewwneesewweswnenwneenwwnwse", "eswswwswnenewseseswnwseswswnewswswsesw", 
+"wewneseeswwwnwneewnwswwswswwsesw", "swseeewseswwswswneswswnwswsw", "seewswwswwswsenwnwswwswnewswswswsww", "seswswswsesesenewswswsenwswswswneswswsw", "enwwwwswswwwswsewswnwnwseeswnwwsw", "seneseneswseswseseswwwsweseswnwesesese", "eswnesweswswswswnwswswswswwwsw", "eeeeweeseseneewe", "swnenwenwnwnenesewwswnwnwnwnwne", "seswneswswwwsewenewwswwswwwww", "seswseseswswswswswsenwsesenwneeewseswswse", "wwnwnwwwenwwnwnww", "seswseeseeseeneesesesewnese", "wnwnwwnesewwwswewsesewnwewwe", "swnwseswswswnweswneweswswswswswswnesw", "swseseseseseswswseswne", "nwswwwwnwswnwsesewenewwsesenwnenw", "swwnwsweeewnwnwwsweseswsw", "nwnwnwnweswnwsenwnwnwnwnwnwnwnww", "eswnenwswnwneeneee", "enweneeeeeeesenenee", "nwwnwweneeseenenenwnwnwwswswnenenw", "eeeneseesweeeewenwesweeee", "swswnewswwsesenwwswswwwwewneswswnesw", "seseseseseseseesesewesesewe", "neswwwwwneswwswwwwsewneneww", "wsewwwwwwnwnwnwwnwnww", "nenwneneneneneseneneenenenewneswnwesene", "swnwseseswswswseweswsweswswnwnesewseesw", "weswswneswseewwnwnwneseesese", "eewwnwnweswww", "swwwwwwwswsewwewswnewwneww", "senesesesenwnwnwnenewsenwnwnewnw", "nwnenwnwnenenwewseenwnwnenwnwwnewnw", "wwwweswwseneneseenwseswnwwnwwwnw", "nwnwwswnenwnwnwwwseewswnwnenweww", "neswnwnewneneeneneneneneneneneseswnesene", "eeenwewseeenwsewnwsewe", "nwnwwnwwnesenwnwnwnwnwnwsenwnwswnenwnw", "nwseswswswnewnenwnwneneenenwwneneeesw", "nwnwnwnwwswsenweenwnweenwnwnwswnwnww", "nwesweeeeenweeseweeeeewee", "swseseswseswswswswnwnwswswsweneswswsese", "swesesweeswswswnwwswswswsewse", "wswswweseswswswenwswnwswswnwwwwsw", "wsesesesenwswswnwseseseeseseesesesese", "wsenewwwwwwwwnewsew", "ewswwwwswwwwwnwweswww", "enwwnwnenwesewne", "wswswseswsesesenesesesese", "swseseseswseseseswswnewe", "swswnwnenenwswneneneneenenenenenwswnenw", "eneeseeseenewseeseewe", "nwneseswswswswswnwswswswswwneeseswswne", "swswsesesesenwseseesenew", "newnwsenwsenenwnwnenwnwnwnesenwwnwnw", "eneenenewwneneneneneneseneenesene", "wnwseweeenwswsewwneswwneenewewe", "nwnwnwwwnwwneenwsenenwseswnwswwnwnw", "nwnwswneneswnwnwnenwwnweswswwnwnenwnw", "neneenweseswseeewneweneenweseee", "eeneweseeeeenwsenweneeeeswswe", "ewsewswnwwwwnwneewseswneenewswwe", "eeweeesweeeneeeeswswseenwnwee", "nwnweswswsewwsenesweswwnwnwenwswe", "wwewwsesenwswnwwsenwnenwwnwsewwnw", "eewsewwnewwwwneswwwswswswww", "enwewswesenweenwneewseswseeee", "sesenewswwwweneneseeswnwnenwneee", "nwewnwsewnwnenwwnewwse", "eseeseseeesesesenwseneswneneswesew", "wswwseenewswenwwweswswnenww", "neseswseseeswswswwswswnwsenwsweseswswse", "eeswwenwnenwneeweseweswneswene", "nenwnenwnwnwneneswnenenenwneenenwswseswnw", "swneeeeseenweneseeweswneesenwenwsw", "nwnwnwsenwwnwsenwnwne", "newwnwsenwnewsewswnenwswwenenwnesese", "senwsenwnesenwnewnenwne"]
+// array, every second row has an offset
+let floor = []
 
 function start() {
-    let total = 0
-    for (let i = 0; i < input.length; i++) {
-        
+    // vul array
+    let size = 41 // longest input is 41, less is prob enough
+    for (let i = 0; i < size; i++) {
+        floor.push([])
+        for (let j = 0; j < size; j++) {
+            // boolean, false white, true black
+            floor[i].push(false)
+        }
     }
-    console.log(total)
+
+    for (let i = 0; i < input.length; i++) {
+        // reference tile, middle
+        let x = 20
+        let y = 20
+        
+        // navigate to tile
+        for (let j = 0; j < input[i].length; j++) {
+            let dir = input[i].substring(j, j+1)
+            if (dir == "e") {
+                x++
+            } else if (dir == "s" && input[i].substring(j+1, j+2) == "e") {
+                if (y % 2 == 0) {
+                    y++
+                } else {
+                    y++
+                    x++
+                }
+                j++
+            } else if (dir == "s" && input[i].substring(j+1, j+2) == "w") {
+                if (y % 2 == 0) {
+                    y++
+                    x--
+                } else {
+                    y++
+                }
+                j++
+            } else if (dir == "w") {
+                x--
+            } else if (dir == "n" && input[i].substring(j+1, j+2) == "e") {
+                if (y % 2 == 0) {
+                    y--
+                } else {
+                    y--
+                    x++
+                }
+                j++
+            } else if (dir == "n" && input[i].substring(j+1, j+2) == "w") {
+                if (y % 2 == 0) {
+                    y--
+                    x--
+                } else {
+                    y--
+                }
+                j++
+            }
+        }
+
+        // flip
+        floor[y][x] = !floor[y][x]
+    }
+    console.log(floor);
+
+    // count black tiles
+    let total = 0
+    for (let i = 0; i < floor.length; i++) {
+        for (let j = 0; j < floor[i].length; j++) {
+            if (floor[i][j]) {
+                total++
+            }
+        }
+    }
+    console.log(total);
 }
 
 start()
 
-// eerste deel: 
+// eerste deel: 282
 // tweede deel: 
