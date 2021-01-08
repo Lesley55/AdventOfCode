70845439572901255566a3b16a8b9f3c805fe1a6
Lesley <hoiLesley55@gmail.com>
2021-01-09 00:58:55 +0100

    day 1, learning python

--- 2015/1/python.py
+++ 2015/1/python.py
@@ -2,11 +2,18 @@ input = open("input.txt")
 input = input.read()
 
 floor = 0
+index = 0
+first = True
 
 for x in input:
+    if floor == -1:
+        first = False
     if x == "(":
         floor += 1 # beetje jammer dat je bij python niet gwn floor++ kan doen
     else:
         floor -= 1
+    if first:
+        index += 1
 
 print(floor)
+print(index)
