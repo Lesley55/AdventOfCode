d27741a9419fe8f6b459cfdd1d618bfb4376a1f7
Lesley <hoiLesley55@gmail.com>
2021-06-03 19:17:19 +0200

    2015 day 23 part 2

--- 2015/23/part1.py
+++ 2015/23/part1.py
@@ -1,6 +1,7 @@
 input = ["jio a, +16", "inc a", "inc a", "tpl a", "tpl a", "tpl a", "inc a", "inc a", "tpl a", "inc a", "inc a", "tpl a", "tpl a", "tpl a", "inc a", "jmp +23", "tpl a", "inc a", "inc a", "tpl a", "inc a", "inc a", "tpl a", "tpl a", "inc a", "inc a", "tpl a", "inc a", "tpl a", "inc a", "tpl a", "inc a", "inc a", "tpl a", "inc a", "tpl a", "tpl a", "inc a", "jio a, +8", "inc b", "jie a, +4", "tpl a", "inc a", "jmp +2", "hlf a", "jmp -7"]
 
 register = {"a": 0, "b": 0}
+register = {"a": 1, "b": 0}
 
 i = 0
 while True:
@@ -29,3 +30,4 @@ while True:
 print(register["b"])
 
 # part 1: 170
+# part 1: 247
