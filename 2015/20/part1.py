1f2f696088a4adad6454628f69188c081ca2fed1
Lesley <hoiLesley55@gmail.com>
2021-06-03 17:44:15 +0200

    2015 day 20 part 1

--- 2015/20/part1.py
+++ 2015/20/part1.py
@@ -1,26 +1,18 @@
-import math
+from functools import reduce
 
 input = 34000000
 
+def factors(n):    
+    return set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
 y = 1
 while True:
-    def divSum(num) :
-        result = 0
-        i = 2
-        while i<= (math.sqrt(num)) :
-            if (num % i == 0) :
-                if (i == (num / i)) :
-                    result = result + i
-                else :
-                    result = result + (i + num/i)
-            i = i + 1
-        return (result + 1)
-    presents = divSum(y)
+    presents = sum(factors(y))
 
     if presents * 10 > input:
         print(y)
         break
     y += 1
 
-# part 1: 831600 to high
-#         1048320
+# part 1: 786240
