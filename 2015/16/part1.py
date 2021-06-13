5c598ced9625603e2108724adc64f4a40764d071
Lesley <hoiLesley55@gmail.com>
2021-06-13 13:33:18 +0200

    2015 day 16 part 1: maybe i was overthinking first, but this feels to easy

--- 2015/16/part1.py
+++ 2015/16/part1.py
@@ -4,21 +4,8 @@ mf = {"children": 3, "cats": 7, "samoyeds": 2, "pomeranians": 3, "akitas": 0, "v
 inp = []
 
 for i in input:
-    inp.append(i)
-    
-print(len(inp))
+    j = i.replace("\n", "").replace(":", "").replace(",", "").split()
+    if j[2] in mf and int(j[3]) == mf[j[2]] and j[4] in mf and int(j[5]) == mf[j[4]] and j[6] in mf and int(j[7]) == mf[j[6]]:
+        print(j[1])
 
-i = 0
-while i < len(inp):
-    j = inp[i].replace(":", "").replace(",", "").split()
-    for m in mf:
-        if (m == j[2] and mf[m] > int(j[3])) or (m == j[4] and mf[m] > int(j[5])) or (m == j[6] and mf[m] > int(j[7])):
-            inp.remove(inp[i])
-            i -= 1
-            break
-    i += 1
-
-print(len(inp))
-print(inp)
-
-# part 1: guessed 430 to high
+# part 1: 40
