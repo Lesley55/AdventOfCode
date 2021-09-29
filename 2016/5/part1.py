1e4f49d352f187608060b1e8d169b36d2d5842b2
Lesley <hoiLesley55@gmail.com>
2021-09-29 14:52:54 +0200

    2016 day 5 part 2

--- 2016/5/part1.py
+++ 2016/5/part1.py
@@ -1,5 +1,20 @@
-input = open("input.txt")
+import hashlib
 
+input = "ffykfhsq"
+id = 0
+password = ""
 
+while True:
+    door = input + str(id)
+    result = hashlib.md5(door.encode())
+    hex = result.hexdigest()
+    if hex[0:5] == "00000":
+        password += str(hex[5:6])
 
-# part 1: 
+    if len(password) >= 8:
+        break
+    id += 1
+
+print(password)
+
+# part 1: c6697b55
