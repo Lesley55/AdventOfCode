7e5259d389115285a962428a4ea870dfb137a44f
Lesley <hoiLesley55@gmail.com>
2021-11-14 21:24:52 +0100

    2016 day 9 part 1

--- 2016/9/part1.py
+++ 2016/9/part1.py
@@ -1,8 +1,25 @@
 input = open("input.txt")
 
-for i in input:
-    pass
+for i in input: # only 1 line
 
-print()
+    length = 0
+    start = None
 
-# part 1:
+    j = 0
+    while j < len(i):
+        if i[j] == "(":
+            start = j
+        elif i[j] == ")" and start is not None:
+            par = i[start+1:j]
+            par = par.split('x')
+            length += int(par[0]) * int(par[1])
+            j += int(par[0])
+            start = None
+        elif start is None:
+            length += 1
+        
+        j += 1
+    
+    print(length)
+
+# part 1: 120765
