e8c0b35b7b2d74191413f982e777840414dea6d5
Lesley <hoiLesley55@gmail.com>
2021-11-23 15:41:13 +0100

    2016 day 12 part 1

--- 2016/12/part1.py
+++ 2016/12/part1.py
@@ -1,8 +1,37 @@
 input = open("input.txt")
 
+cmd = []
+
 for i in input:
-    pass
+    i = i.strip().split()
+    cmd.append(i)
 
-print()
+r = ["a", "b", "c", "d"]
+registers = [0,0,0,0]
 
-# part 1:
+i = 0
+while i < len(cmd):
+    if cmd[i][0] == "cpy":
+        if cmd[i][1].isnumeric():
+            registers[r.index(cmd[i][2])] = int(cmd[i][1])
+        else:
+            registers[r.index(cmd[i][2])] = registers[r.index(cmd[i][1])]
+    elif cmd[i][0] == "inc":
+        registers[r.index(cmd[i][1])] += 1
+    elif cmd[i][0] == "dec":
+        registers[r.index(cmd[i][1])] -= 1
+    elif cmd[i][0] == "jnz":
+        zero = False
+        if cmd[i][1].isnumeric():
+            if cmd[i][1] == 0:
+                zero = True
+        elif registers[r.index(cmd[i][1])] == 0:
+                zero = True
+        if not zero:
+            i += int(cmd[i][2])
+            continue
+    i += 1
+
+print(registers[r.index("a")])
+
+# part 1: 318009
