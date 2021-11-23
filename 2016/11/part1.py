50b6330d379d99e4f634162e355d045dba379006
Lesley <hoiLesley55@gmail.com>
2021-11-23 14:56:26 +0100

    ?

--- 2016/11/part1.py
+++ 2016/11/part1.py
@@ -1,14 +1,14 @@
-input = open("input.txt")
+# input = open("input.txt")
 
-for i in input:
-    pass
+# for i in input:
+#     pass
 
 # it doesn't matter if its a chip or a generator, i have to find the pair, so giving both the same number
 floors = [
     [],
     [3],
-    [3,4,4,5,5],
-    [1,1,2,2]
+    [3, 4, 4, 5, 5],
+    [1, 1, 2, 2]
 ]
 
 # test
@@ -16,11 +16,13 @@ floors = [
     [],
     [2],
     [1],
-    [1,2]
+    [1, 2]
 ]
 
+# ik wilde eerst, kijken welke mogelijke stappen de lift kan doen en dan recursief verder, maar dan is het ook mogelijk heen en weer te gaan, en blijft hij oneindig doorgaan
+# geen idee hoe ik dit moet aanpakken
 
 
-print(floors)
+print()
 
 # part 1:
