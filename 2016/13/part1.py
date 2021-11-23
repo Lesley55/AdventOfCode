ee477a94c9c3f0abadcf9cb9e3a5fd8c60aebbc7
Lesley <hoiLesley55@gmail.com>
2021-11-23 16:30:32 +0100

    2016 day 13 part 1

--- 2016/13/part1.py
+++ 2016/13/part1.py
@@ -1,8 +1,48 @@
-input = open("input.txt")
+input = 1358
 
-for i in input:
-    pass
+def isOpen(x, y):
+    number = (x * x) + (3 * x) + (2 * x * y) + y + (y * y)
+    number += input
+    binary = bin(number)
+    one = 0
+    for b in binary:
+        if b == "1":
+            one += 1
+    return one % 2 == 0
 
-print()
+visited = {}
+possible = []
+destination = False
 
-# part 1:
+def rec(x, y, steps):
+    global destination
+
+    # dont visit multiple times, which could create loops
+    if (x,y) in visited:
+        return
+
+    # check if destination
+    if x == 31 and y == 39:
+        print(steps)
+        destination = True
+
+    # add to visited
+    visited[(x,y)] = True
+
+    steps += 1
+    # check if possible to set a step for every direction
+    if isOpen(x+1, y):
+        possible.append([x+1, y, steps])
+    if isOpen(x, y+1):
+        possible.append([x, y+1, steps])
+    if isOpen(x-1, y):
+        possible.append([x-1, y, steps])
+    if isOpen(x, y-1):
+        possible.append([x, y-1, steps])
+
+rec(1, 1, 0) # starting point
+while not destination:
+    for i in possible:
+        rec(i[0], i[1], i[2])
+
+# part 1: 96
