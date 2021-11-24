7bef7b62c2474e9fe7ce6357b2b6ba85ada1b0b4
Lesley <hoiLesley55@gmail.com>
2021-11-24 19:44:17 +0100

    2016 day 14 part1: wrong code right answer at wrong index

--- 2016/14/part1.py
+++ 2016/14/part1.py
@@ -1,8 +1,44 @@
-input = open("input.txt")
+import hashlib
 
-for i in input:
-    pass
+input = "qzyelonm"
+input = "abc"
+index = 0
+found = 0
 
-print()
 
-# part 1:
+def hash(str2hash):
+    result = hashlib.md5(str2hash.encode())
+    return result.hexdigest().lower()
+
+
+def next1000(r):
+    for j in range(1000):
+        result2 = hash(input + str(index + 1 + j))
+        for k in range(len(result2)-4):
+            if r == result2[k] and r == result2[k+1] and r == result2[k+2] and r == result2[k+3] and r == result2[k+4]:
+                print(index, found)
+                # print(result2)
+                return True
+    return False
+
+
+def check(result):
+    global found
+    for i in range(len(result)-2):
+        r = result[i]
+        if r == result[i+1] and r == result[i+2]:
+            if next1000(r):
+                # print(result)
+                found += 1
+                return
+
+
+# while found < 64:
+while found < 68:
+    result = hash(input + str(index))
+    check(result)
+    index += 1
+
+print(index-1)
+
+# part 1: 15168 but in wrong place index 66 and example at index 67
