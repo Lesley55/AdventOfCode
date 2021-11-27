fe737710c15fa98ba198c6861f832b68f5682c4c
Lesley <hoiLesley55@gmail.com>
2021-11-27 23:58:01 +0100

    2016 day 16 part 1

--- 2016/16/part1.py
+++ 2016/16/part1.py
@@ -1,8 +1,27 @@
-input = open("input.txt")
+input = "11101000110010100"
+disk = 272
 
-for i in input:
-    pass
+while len(input) < disk:
+    reverse = input[::-1]
+    new = ""
+    for i in reverse:
+        if i == "0":
+            new += "1"
+        else:
+            new += "0"
+    input += "0" + new
 
-print()
+checksum = input[:disk]
 
-# part 1:
+while len(checksum) % 2 == 0:
+    newsum = ""
+    for i in range(0, len(checksum), 2):
+        if checksum[i] == checksum[i + 1]:
+            newsum += "1"
+        else:
+            newsum += "0"
+    checksum = newsum
+
+print(checksum)
+
+# part 1: 10100101010101101
