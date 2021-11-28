bd8c8db2ace51124606e37ba8dee6ce0871bab1d
Lesley <hoiLesley55@gmail.com>
2021-11-28 01:42:10 +0100

    2016 day 17 part 2

---
+++ 2016/17/part2.py
@@ -0,0 +1,33 @@
+import hashlib
+
+passcode = "hhhxzeay"
+
+open = "bcdef"
+possible = [[0,0,passcode]]
+
+def nextDoor(x, y, input):
+    if x < 0 or y < 0 or 4 <= x or 4 <= y:
+        return False
+    elif x == 3 and y == 3:
+        return True
+
+    result = hashlib.md5(input.encode())
+    hex = result.hexdigest()
+    four = hex[0:4]
+
+    for i in range(len(four)):
+        if four[i] in open:
+            if i == 0:
+                possible.append([x, y - 1, input + "U"])
+            elif i == 1:
+                possible.append([x, y + 1, input + "D"])
+            elif i == 2:
+                possible.append([x - 1, y, input + "L"])
+            elif i == 3:
+                possible.append([x + 1, y, input + "R"])
+
+for i in possible:
+    if nextDoor(i[0], i[1], i[2]):
+        print(len(i[2]) - len(passcode))
+
+# part 2: 398
