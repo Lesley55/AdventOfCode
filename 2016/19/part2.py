1af29e275214cc80698eb32fc4d77e87ed35386c
Lesley <hoiLesley55@gmail.com>
2021-11-28 21:42:48 +0100

    2016 day 19 part 2

---
+++ 2016/19/part2.py
@@ -0,0 +1,25 @@
+import math
+
+input = 3001330
+
+elves = []
+
+for i in range(input):
+    elves.append(i)
+
+current = 0
+while len(elves) > 1:
+    l = len(elves)
+    if current < l:
+        opposite = current + math.floor(l / 2)
+        if opposite >= l:
+            opposite -= l
+        elves.pop(opposite)
+        if opposite > current:
+            current += 1
+    else:
+        current = 0
+
+print(elves[0] + 1)
+
+# part 2:
