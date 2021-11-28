a3796d4c5362cb94267944ca43465d1dd8f04051
Lesley <hoiLesley55@gmail.com>
2021-11-28 23:49:20 +0100

    2016 day 22 part 1

--- 2016/22/part1.py
+++ 2016/22/part1.py
@@ -1,8 +1,19 @@
 input = open("input.txt")
 
+df = []
+
 for i in input:
-    pass
+    j = i.strip().split()
+    k = j[0].split("-")
+    df.append([int(j[1][:-1]), int(j[2][:-1]), int(j[3][:-1]), int(j[4][:-1])])
 
-print()
+viable = 0
 
-# part 1:
+for i in range(len(df)):
+    for j in range(len(df)):
+        if i != j and df[i][1] > 0 and df[i][1] <= df[j][2]:
+            viable += 1
+
+print(viable)
+
+# part 1: 981
