abd46e9f3403213ca872fbf3777cf6d32781d12a
Lesley <hoiLesley55@gmail.com>
2021-11-28 18:19:55 +0100

    2016 day 19 part 1

--- 2016/19/part1.py
+++ 2016/19/part1.py
@@ -1,8 +1,30 @@
-input = open("input.txt")
+input = 3001330
 
-for i in input:
-    pass
+elves = {}
 
-print()
+for i in range(input):
+    elves[i] = True
 
-# part 1:
+current = 0
+next = current + 1
+while len(elves) > 1:
+    if current < input:
+        if current in elves:
+            if next < input:
+                if next in elves and next != current:
+                    elves.pop(next)
+                    current = next + 1
+                    next = current + 1
+                else:
+                    next += 1
+            else:
+                next = 0
+        else:
+            current += 1
+    else:
+        current = 0
+
+print(elves)
+print("+1 because elves start from 1 not 0")
+
+# part 1: 1808357
