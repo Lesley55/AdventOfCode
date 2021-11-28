69d3785078605d752f59c60f15ccc672b34adc96
Lesley <hoiLesley55@gmail.com>
2021-11-28 21:58:26 +0100

    2016 day 20 part 1

--- 2016/20/part1.py
+++ 2016/20/part1.py
@@ -1,8 +1,25 @@
 input = open("input.txt")
 
+blacklist = []
+
 for i in input:
-    pass
+    i = i.strip().split("-")
+    i = [int(i[0]), int(i[1])]
+    blacklist.append(i)
 
-print()
+ip = 0
 
-# part 1:
+def check():
+    global ip
+    for i in blacklist:
+        if i[0] <= ip and ip <= i[1]:
+            ip = i[1] + 1
+            return False
+    return True
+
+while ip < 4294967295:
+    if check():
+        print(ip)
+        break
+
+# part 1: 19449262
