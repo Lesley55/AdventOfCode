22dd69f4fada096cd231b9030aa4cb6e8f6b68c6
Lesley <hoiLesley55@gmail.com>
2021-12-02 01:10:44 +0100

    2021 day 1 part 2

---
+++ 2021/1/part2.py
@@ -0,0 +1,18 @@
+input = open("input.txt")
+
+depth = []
+for i in input:
+    depth.append(int(i))
+
+sums = []
+for i in range(2, len(depth)):
+    sums.append(depth[i-2] + depth[i-1] + depth[i])
+
+larger = 0
+for i in range(1, len(sums)):
+    if sums[i] > sums[i-1]:
+        larger += 1
+
+print(larger)
+
+# part 2: 
