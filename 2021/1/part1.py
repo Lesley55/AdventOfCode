55614d28510f38188d6b09789b22aaebd1572cc3
Lesley <hoiLesley55@gmail.com>
2021-12-02 01:06:24 +0100

    2021 day 1 part 1

---
+++ 2021/1/part1.py
@@ -0,0 +1,14 @@
+input = open("input.txt")
+
+depth = [100000]
+for i in input:
+    depth.append(int(i))
+
+larger = 0
+for i in range(1, len(depth)):
+    if depth[i] > depth[i-1]:
+        larger += 1
+
+print(larger)
+
+# part 1: 1581
