f685119532f1e8f9798045ee80045e1986dc2b9b
Lesley <hoiLesley55@gmail.com>
2021-12-09 16:42:45 +0100

    2021 day 9 part 2

--- 2021/9/part1.php
+++ 2021/9/part1.php
@@ -38,15 +38,68 @@ function allBigger($i, $j) {
 }
 
 $total = 0;
+$lowest = [];
 for ($i = 0; $i < count($cave); $i++) {
     for ($j = 0; $j < count($cave[$i]); $j++) {
         if (allBigger($i, $j)) {
             $total += 1 + $cave[$i][$j];
+            array_push($lowest, [$i, $j]);
         }
     }
 }
 
-echo $total;
+echo "part 1: " . $total . "<br>";
 
 // part 1: 465
+
+
+
+function notIn($arr, $array) {
+    for ($i = 0; $i < count($array); $i++) {
+        if ($arr == $array[$i]) {
+            return False;
+        }
+    }
+    return True;
+}
+
+$prev = [];
+function around($i, $j) {
+    global $cave;
+    global $prev;
+    array_push($prev, [$i, $j]);
+    $size = 1;
+    if (array_key_exists($i+1, $cave)) {
+        if ($cave[$i+1][$j] < 9 and notIn([$i+1, $j], $prev)) {
+            $size += around($i+1, $j);
+        }
+    }
+    if (array_key_exists($i-1, $cave)) {
+        if ($cave[$i-1][$j] < 9 and notIn([$i-1, $j], $prev)) {
+            $size += around($i-1, $j);
+        }
+    }
+    if (array_key_exists($j+1, $cave[$i])) {
+        if ($cave[$i][$j+1] < 9 and notIn([$i, $j+1], $prev)) {
+            $size += around($i, $j+1);
+        }
+    }
+    if (array_key_exists($j-1, $cave[$i])) {
+        if ($cave[$i][$j-1] < 9 and notIn([$i, $j-1], $prev)) {
+            $size += around($i, $j-1);
+        }
+    }
+    return $size;
+}
+
+$basins = [];
+for ($i = 0; $i < count($lowest); $i++) {
+    $size = around($lowest[$i][0], $lowest[$i][1]);
+    array_push($basins, $size);
+}
+
+rsort($basins);
+echo "part 2: " . $basins[0] * $basins[1] * $basins[2];
+
+// part 2: 1269555
 ?>
