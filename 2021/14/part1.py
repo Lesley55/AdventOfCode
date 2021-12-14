82c8de27d272ea6fffb239790ae7385ff8301e1e
Lesley <hoiLesley55@gmail.com>
2021-12-14 14:17:26 +0100

    2021 day 14 part 2

--- 2021/14/part1.py
+++ 2021/14/part1.py
@@ -1,25 +1,41 @@
-from collections import Counter
-
 input = open("input.txt")
-template = [char for char in "BSONBHNSSCFPSFOPHKPK"]
-steps = 10
+template = "BSONBHNSSCFPSFOPHKPK"
+steps = 10 # part 1
+steps = 40 # part 2
 
+amount = {}
 insert = {}
 for i in input:
     i = i.strip().split(" -> ")
-    insert[i[0]] = i[1]
+    amount[i[0]] = 0
+    insert[i[0]] = [i[0][0]+i[1], i[1]+i[0][1]]
+
+for i in range(len(template)-1):
+    amount[template[i:i+2]] += 1
 
 for s in range(steps):
-    new = []
-    for i in range(len(template)-1):
-        substr = template[i] + template[i+1]
-        new.append(template[i])
-        if substr in insert:
-            new.append(insert[substr])
-    new.append(template[-1])
-    template = new
+    new = {}
+    for i in amount:
+        for j in insert[i]:
+            if j in new:
+                new[j] += amount[i]
+            else:
+                new[j] = amount[i]
+    amount = new
 
-occ = Counter(template)
+start = template[:2]
+for s in range(steps):
+    start = insert[start][0]
+
+occ = {}
+occ[start[0]] = amount[start]
+for i in amount:
+    if i[1] in occ:
+        occ[i[1]] += amount[i]
+    else:
+        occ[i[1]] = amount[i]
+
 print(max(occ.values()) - min(occ.values()))
 
 # part 1: 2740
+# part 2: 2959788056211
