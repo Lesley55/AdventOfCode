ca513d2623be15a18d6e8b31e296a64b07f83585
Lesley <hoiLesley55@gmail.com>
2021-12-15 17:43:28 +0100

    2021 day 15 part 1

--- 2021/15/part1.py
+++ 2021/15/part1.py
@@ -7,23 +7,29 @@ for i in input:
     i = i.strip()
     grid.append([int(char) for char in i])
 
-lowest = 9999999999999999
-def recursive(x, y, risk, prev):
-    global lowest
-    if y == len(grid) and x == len(grid[-1]):
-        if risk < lowest:
-            lowest = risk
-        return
-    
-    previous = copy.copy(prev)
-    previous.append([x,y])
-    possible = [[x + 1, y], [x - 1, y], [x, y - 1], [x, y + 1]]
-    for i in possible:
-        if not [i[0], i[1]] in prev and 0 <= i[1] and i[1] < len(grid) and 0 <= i[0] and i[0] < len(grid[i[1]]):
-            recursive(i[0], i[1], risk + grid[i[1]][i[0]], previous)
+dist = []
+for i in grid:
+    dist.append([])
+    for j in i:
+        dist[-1].append(9999999999)
+dist[0][0] = 0
 
-recursive(0, 0, 0, [])
+# could be more efficient, but i wanted to code something myself and not just implement the algoritm
+while True:
+    distance = copy.deepcopy(dist)
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            possible = [[x + 1, y], [x - 1, y], [x, y - 1], [x, y + 1]]
+            for i in possible:
+                if 0 <= i[1] and i[1] < len(grid) and 0 <= i[0] and i[0] < len(grid[i[1]]):
+                    d = distance[y][x] + grid[i[1]][i[0]]
+                    if d < distance[i[1]][i[0]]:
+                        distance[i[1]][i[0]] = d
+    if dist == distance:
+        dist = distance
+        break
+    dist = distance
 
-print(lowest)
+print(dist[-1][-1])
 
-# part 1: 
+# part 1: 613
