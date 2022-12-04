29703198b900d9de54dc18ec733660644c5a41ab
Lesley <hoiLesley55@gmail.com>
2022-12-04 14:27:37 +0100

    aoc 2022 day 4 part 2

--- 2022/4/part1.py
+++ 2022/4/part1.py
@@ -1,12 +1,17 @@
 input = open("input.txt")
 
 contains = 0
+contains2 = 0
 for i in input:
     i = i.strip().split(",")
     a, b, c, d = i[0].split("-") + i[1].split("-")
     a, b, c, d = int(a), int(b), int(c), int(d)
     if((a >= c and b <= d) or (a <= c and b >= d)):
         contains += 1
+    if((a >= c and a <= d) or (b <= c and b >= d) or (c >= a and c <= b) or (d <= a and d >= b)):
+        contains2 += 1
 
 print(contains)
 # part 1: 444
+print(contains2)
+# part 2: 801
