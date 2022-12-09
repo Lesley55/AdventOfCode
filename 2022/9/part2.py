41c2f4e33eaf8082a1059dc2e94dce7356924b13
Lesley <hoiLesley55@gmail.com>
2022-12-09 15:20:45 +0100

    aoc 2022 day 9 part 2

--- 2022/9/part2.py
+++ 2022/9/part2.py
@@ -10,7 +10,7 @@ for i in input:
         rope[0] = [rope[0][0] + step[i[0]][0], rope[0][1] + step[i[0]][1]]
         for k in range(1, len(rope)):
             d0, d1 = rope[k - 1][0] - rope[k][0], rope[k - 1][1] - rope[k][1]
-            if (abs(d0) > 1 and d1 == 0) or (abs(d1) > 1 and d0 == 0):
+            if (abs(d0) > 1 and d1 == 0) or (abs(d1) > 1 and d0 == 0) or (abs(d0) > 1 and abs(d1) > 1):
                 rope[k] = (int(rope[k][0] + (d0 / 2)), int(rope[k][1] + (d1 / 2)))
             elif abs(d0) > 1:
                 rope[k] = (int(rope[k][0] + (d0 / 2)), int(rope[k][1] + d1))
@@ -18,11 +18,5 @@ for i in input:
                 rope[k] = (int(rope[k][0] + d0), int(rope[k][1] + (d1 / 2)))
         distinct.add(rope[-1])
 
-# vis = [["." for v in range(22)] for w in range(22)]
-# for q in distinct:
-#     vis[q[0] + 11][q[1] + 11] = "#"
-# for p in vis:
-#     print("".join(p))
-
 print(len(distinct))
-# part 2: 2428 wrong
+# part 2: 2445
