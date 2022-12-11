53844e5a5152f65b9a714bdcc43ecf812f893c2c
Lesley <hoiLesley55@gmail.com>
2022-12-11 16:52:40 +0100

    aoc 2022 day 11 part 1

---
+++ 2022/11/part1.py
@@ -0,0 +1,56 @@
+input = open("input.txt")
+
+monkeys = []
+for i in input:
+    i = i.strip().split(" ")
+    if len(i) < 2: continue
+    elif i[0] == "Starting":
+        monkeys.append({"inspected": 0})
+        monkeys[-1]["items"] = []
+        for j in i[2:]:
+            monkeys[-1]["items"].append(int(j.replace(",", "")))
+    elif i[0] == "Operation:":
+        monkeys[-1]["op"] = i[3:]
+    elif i[0] == "Test:":
+        monkeys[-1]["test"] = int(i[3])
+    elif i[1] == "true:":
+        monkeys[-1]["t"] = int(i[5])
+    elif i[1] == "false:":
+        monkeys[-1]["f"] = int(i[5])
+
+rounds = 20
+for i in range(rounds):
+    for monkey in monkeys:
+        for j in range(len(monkey["items"])):
+            item = monkey["items"].pop(0)
+            # inspect
+            a = 0
+            b = 0
+            if monkey["op"][0] == "old":
+                a = item
+            else:
+                a = int(monkey["op"][0])
+            if monkey["op"][2] == "old":
+                b = item
+            else:
+                b = int(monkey["op"][2])
+            if monkey["op"][1] == "+":
+                item = a + b
+            elif monkey["op"][1] == "*":
+                item = a * b
+            monkey["inspected"] += 1
+            # relief
+            item = int(item / 3)
+            # test and throw
+            if item % monkey["test"] == 0:
+                monkeys[monkey["t"]]["items"].append(item)
+            else:
+                monkeys[monkey["f"]]["items"].append(item)
+
+inspected = []
+for i in monkeys:
+    inspected.append(i["inspected"])
+inspected.sort()
+
+print(inspected[-1] * inspected[-2])
+# part 1: 54253
