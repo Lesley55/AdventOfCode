d0280005807c688873d743daf076fba29f753df8
Lesley <hoiLesley55@gmail.com>
2022-12-16 16:39:55 +0100

    aoc 2022 day 16 part 1: better, but still takes to long

--- 2022/16/part1.py
+++ 2022/16/part1.py
@@ -1,5 +1,3 @@
-import copy
-
 input = open("input.txt")
 
 valves = {}
@@ -7,23 +5,32 @@ for i in input:
     i = i.strip().replace("=", " ").replace(";", "").replace(",", "").split()
     valves[i[1]] = (int(i[5]), i[10:])
 
+dist = {}
+def get_path_lengths(start, current, steps, visited):
+    if not current in visited:
+        if steps > 0 and valves[current][0] > 0:
+            dist[start].append([current, steps])
+        for i in valves[current][1]:
+            get_path_lengths(start, i, steps + 1, visited + [current])
+
+for i in valves:
+    dist[i] = []
+    get_path_lengths(i, i, 0, [])
+
 most = 0
-def next(current, time, open, released, visited):
+def next(current, time, open, released):
     global most
     if released > most:
         most = released
-        print(open, released)
-    if time == 0:
+        print(released, open)
+    if time <= 0:
         return
-    open, released, visited = copy.copy(open), copy.copy(released), copy.copy(visited)
-    for i in valves[current][1]:
-        if not current + i in visited:
-            next(i, time - 1, open, released, visited + [current + i])
-    if valves[current][0] != 0 and current not in open:
-        released += (time - 1) * valves[current][0]
-        open.append(current)
-        next(current, time - 1, open, released, visited)
+    for i in dist[current]:
+        if i[0] != current:
+            next(i[0], time - i[1], open, released)
+    if not current in open:
+        next(current, time - 1, open + [current], released + (time - 1) * valves[current][0])
 
-next("AA", 30, [], 0, [])
+next("AA", 30, [], 0)
 print(most)
 # part 1: 
