19f2714d127ba88095c4ba47068bd0458be15a1d
Lesley <hoiLesley55@gmail.com>
2022-12-22 15:44:15 +0100

    aoc 2022 day 21 part 2: hacky solution, maybe i should come back and code something better later

--- 2022/21/part2.py
+++ 2022/21/part2.py
@@ -20,15 +20,22 @@ def equal(monkey):
                     b = str(monkey[a[0]]) + a[1] + str(monkey[a[2]])
                     monkey[m] = eval(b)
     root = inp["root"].split()
+    # print(monkey[root[0]], monkey[root[2]])
     return monkey[root[0]] == monkey[root[2]]
 
-i = 0
+humn = 3952673930000
 while True:
     monkey = copy.copy(inp)
-    monkey["humn"] = i / 1
+    monkey["humn"] = humn / 1
     if equal(monkey):
         break
-    i += 1
+    humn += 1
 
-print(i)
-# part 2: 
+# printing root, the second value is always constant: 13439547545467
+# and the first value is slowly decreasing when humn gets higher
+# so i just put a random higher starting value for humn
+# until i got close enough that running the code doesnt take ages
+# would have preferred to code a solution, but at least i got the answer
+
+print(humn)
+# part 2: 3952673930912
