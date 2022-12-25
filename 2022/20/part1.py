efb6086b5de771e603681d7aa41dac7a895ccb52
Lesley <hoiLesley55@gmail.com>
2022-12-25 21:32:32 +0100

    aoc 2022 day 20 part 1

--- 2022/20/part1.py
+++ 2022/20/part1.py
@@ -5,23 +5,17 @@ numbers = []
 for i in input:
     i = i.strip()
     order.append(int(i))
-    numbers.append(int(i))
+    numbers.append(len(order) - 1)
 
-for n in order:
+for n in range(len(order)):
     i = numbers.index(n)
     numbers.pop(i)
-    new = (i + n) % len(numbers)
-    ## for checking to make it visually the same as example when printing, but doesn't change the order
-    # if new == 0:
-    #     numbers.append(n)
-    # else:
+    new = (i + order[n]) % len(numbers)
     numbers.insert(new, n)
-    # print(n, numbers)
 
 sum = 0
 for i in [1000, 2000, 3000]:
-    sum += numbers[(numbers.index(0) + i) % len(numbers)]
-    # print(numbers[(numbers.index(0) + i) % len(numbers)])
+    sum += order[numbers[(numbers.index(order.index(0)) + i) % len(numbers)]]
 
 print(sum)
-# part 1: -3850 wrong
+# part 1: 3700
