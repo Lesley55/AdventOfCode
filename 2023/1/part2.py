d4122019501d5833691168654a0d02f50ca26502
Lesley <hoiLesley55@gmail.com>
2023-12-01 18:55:37 +0100

    2023 day 1 part 2

--- 2023/1/part2.py
+++ 2023/1/part2.py
@@ -2,34 +2,28 @@ input = open("input.txt")
 
 total = 0
 words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+d = ""
+
+def get_number(i, a):
+    global d
+    for j in a:
+        if i[j].isdigit():
+            d += i[j]
+            break
+        found = False
+        for w in words:
+            if i[j:j+len(w)] == w:
+                d += str(words.index(w) + 1)
+                found = True
+                break
+        if found:
+            break
 
 for i in input:
-    d = ""
-    for j in range(len(i)):
-        if i[j].isdigit():
-            d += i[j]
-            break
-        found = False
-        for w in words:
-            if i[j:j+len(w)] == w:
-                d += str(words.index(w) + 1)
-                found = True
-                break
-        if found:
-            break
-    for j in range(len(i))[::-1]:
-        if i[j].isdigit():
-            d += i[j]
-            break
-        found = False
-        for w in words:
-            if i[j:j+len(w)] == w:
-                d += str(words.index(w) + 1)
-                found = True
-                break
-        if found:
-            break
+    get_number(i, range(len(i)))
+    get_number(i, range(len(i))[::-1])
     total += int(d)
+    d = ""
 
 print(total)
 
