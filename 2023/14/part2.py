40f1d0d3263b4e70ee445524406badacd01cc9ee
Lesley <hoiLesley55@gmail.com>
2023-12-14 21:10:44 +0100

    2023 day 14 part 2

--- 2023/14/part2.py
+++ 2023/14/part2.py
@@ -16,20 +16,24 @@ history = []
 cycle = 0
 while cycle < 1000000000:
     cycle += 1
-    print(cycle)
     for d in [[-1, 0], [0, -1], [1, 0], [0, 1]]:
-        # can be improved by starting at the side you want them to roll to, and moving all the way instead of 1 step
-        # now its not very efficient making many unnessesary loops over every rolling stone, 
-        # because the other rolling stones are still in the way
+        if d == [-1, 0]:
+            roll.sort(key = lambda x: x[0])
+        elif d == [0, -1]:
+            roll.sort(key = lambda x: x[1])
+        elif d == [1, 0]:
+            roll.sort(key = lambda x: -x[0])
+        elif d == [0, 1]:
+            roll.sort(key = lambda x: -x[1])
         changed = True
         while changed:
             changed = False
             for rock in range(len(roll)):
                 moved = [roll[rock][0] + d[0], roll[rock][1] + d[1]]
-                if 0 <= moved[0] and 0 <= moved[1] and moved[0] < len(input) and moved[1] < len(input[0]):
-                    if not moved in roll and not moved in stuck:
-                        roll[rock] = moved
-                        changed = True
+                while 0 <= moved[0] and 0 <= moved[1] and moved[0] < len(input) and moved[1] < len(input[0]) and not moved in roll and not moved in stuck:
+                    roll[rock] = moved
+                    changed = True
+                    moved = [moved[0] + d[0], moved[1] + d[1]]
     for h in history:
         if roll == h[1]:
             cycle = 1000000000 - ((1000000000 - h[0]) % (cycle - h[0]))
@@ -45,4 +49,4 @@ for rock in roll:
 
 print(total)
 
-# part 2: 
+# part 2: 104533
