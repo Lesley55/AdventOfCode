7fc7460c03a93274fbad365bc9d4759611487e8b
Lesley <hoiLesley55@gmail.com>
2023-12-14 15:36:59 +0100

    2023 day 14 part 1

---
+++ 2023/14/part2.py
@@ -0,0 +1,40 @@
+input = open("input.txt")
+input = input.readlines()
+
+roll = []
+stuck = []
+for i in range(len(input)):
+    for j in range(len(input[i])):
+        if input[i][j] == "O":
+            roll.append([i, j])
+        if input[i][j] == "#":
+            stuck.append([i, j])
+
+changed = True
+while changed:
+    changed = False
+    for rock in range(len(roll)):
+        up = [roll[rock][0] - 1, roll[rock][1]]
+        if 0 <= up[0] and not up in roll and not up in stuck:
+            roll[rock] = up
+            changed = True
+
+for i in range(len(input)):
+    row = ""
+    for j in range(len(input[i])):
+        if [i, j] in stuck:
+            row += "#"
+        elif [i, j] in roll:
+            row += "O"
+        else:
+            row += "."
+    print(row)
+
+total = 0
+
+for rock in roll:
+    total += len(input) - rock[0]
+
+print(total)
+
+# part 1: 108813
