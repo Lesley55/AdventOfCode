37b964ad704b0dc439e4a0e36c04ad626e2632fc
Lesley Mulder <hoilesley55@gmail.com>
2025-09-11 22:08:08 +0200

    aoc 2017 day 12

---
+++ 2017/12/part2.py
@@ -0,0 +1,32 @@
+input = open("input.txt")
+input = input.readlines()
+
+programs = {}
+for i in input:
+    i = i.strip().split(" <-> ")
+    programs[i[0]] = i[1].split(", ")
+
+old = []
+groups = 0
+for p in programs:
+    if p in old:
+        continue
+    
+    groups += 1
+    visited = []
+    check = [p]
+
+    while len(check) > 0:
+        current = check.pop(0)
+        if current in visited:
+            continue
+        visited.append(current)
+        for i in programs[current]:
+            if not i in visited and not i in check:
+                check.append(i)
+    
+    old += visited
+
+print(groups)
+
+# part 2: 181
