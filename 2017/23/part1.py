1157fff21dafbfbe22293b13d074ef1544c01e25
Lesley Mulder <hoilesley55@gmail.com>
2025-10-09 16:58:18 +0200

    2017 nday 23 part 1

---
+++ 2017/23/part1.py
@@ -0,0 +1,36 @@
+input = open("input.txt")
+input = input.readlines()
+
+registers = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0}
+instructions = []
+for i in input:
+    instructions.append(i.strip().split(" "))
+
+def getValue(a):
+    if not a.isalpha():
+        return int(a)
+    else:
+        return registers[a]
+    
+jump = 0
+invoked = 0
+while True:
+    if jump < 0 or jump >= len(instructions):
+        print("terminated")
+        break
+    if instructions[jump][0] == "set":
+        registers[instructions[jump][1]] = getValue(instructions[jump][2])
+    elif instructions[jump][0] == "sub":
+        registers[instructions[jump][1]] -= getValue(instructions[jump][2])
+    elif instructions[jump][0] == "mul":
+        invoked += 1
+        registers[instructions[jump][1]] *= getValue(instructions[jump][2])
+    elif instructions[jump][0] == "jnz":
+        if getValue(instructions[jump][1]) != 0:
+            jump += getValue(instructions[jump][2])
+            continue
+    jump += 1
+
+print(invoked)
+
+# part 1: 5929
