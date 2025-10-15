367d2abbb15171fa7ea61d59177c70dc909bf91a
Lesley Mulder <hoilesley55@gmail.com>
2025-10-15 17:29:59 +0200

    2017 day 24

---
+++ 2017/24/part1.py
@@ -0,0 +1,38 @@
+input = open("input.txt")
+input = input.readlines()
+
+components = []
+for i in input:
+    i = i.strip().split("/")
+    i[0] = int(i[0])
+    i[1] = int(i[1])
+    components.append(i)
+
+strongest = 0
+
+def r(options, bridge = []):
+    global strongest
+    last = 0 if len(bridge) <= 0 else bridge[-1]
+    for o in range(len(options)):
+        if last == options[o][0]:
+            br = bridge.copy()
+            br += options[o]
+            op = options.copy()
+            op.remove(options[o])
+            r(op, br)
+        elif last == options[o][1]:
+            br = bridge.copy()
+            br += options[o][::-1]
+            op = options.copy()
+            op.remove(options[o])
+            r(op, br)
+    strength = 0
+    for b in bridge:
+        strength += b
+    if strength > strongest:
+        strongest = strength
+
+r(components)
+print(strongest)
+
+# part 1: 2006
